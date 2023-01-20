b2eecb19c03227c5df2eb35c2f6f9127a78afd94
Collin Weaver <weaverscajunrenovations@gmail.com>
2023-01-20 08:41:10 -0600

    first commit, originated database and db methods with team

---
+++ src/db/customerReviews.js
@@ -0,0 +1,51 @@
+const client = require("./client");
+
+/*
+
+Tri Approved
+
+addReviewToCustomer
+getCustomerReviewById
+updateCustomerReview
+deleteCustomerReview
+
+*/
+
+async function addReviewToCustomer() {
+  try {
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function getCustomerReviewById() {
+  try {
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function updateCustomerReview() {
+  try {
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function deleteCustomerReview() {
+  try {
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+module.exports = {
+  addReviewToCustomer,
+  getCustomerReviewById,
+  updateCustomerReview,
+  deleteCustomerReview,
+};
