8e5118b9addb30db064ac9083cc9b6569096483c
Maria Tran <mp.tran828@gmail.com>
2023-01-23 13:54:04 -0600

    customers api

---
+++ src/api/utils.js
@@ -0,0 +1,16 @@
+function requireCustomer(req, res, next) {
+    if (!req.customer) {
+      res.status(401);
+      next({
+        name: "MissingUserError",
+        message: "You must be logged in to perform this action",
+      });
+    }
+  
+    next();
+  }
+  
+  module.exports = {
+    requireCustomer,
+  };
+  
