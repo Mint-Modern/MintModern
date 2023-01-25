e44c9b5c8f6ec2f08dd39bb638efa49aab097833
Collin Weaver <weaverscajunrenovations@gmail.com>
2023-01-25 10:57:15 -0600

    debugged DB and API

--- src/api/customerReviews.js
+++ src/api/customerReviews.js
@@ -6,30 +6,24 @@ const { requireCustomer } = require("./utils");
 // not sure if we would need this if we have a route in reviews??
 
 // DELETE /api/customerReviews/:customerReviewId
-router.delete(
-  "./:customerReviewId",
-  requireCustomer,
-  async (req, res, next) => {
-    const { customerReviewId } = req.params;
+router.delete("/:customerReviewId", requireCustomer, async (req, res, next) => {
+  const { customerReviewId } = req.params;
 
-    const customerReview = await deleteCustomerReview(customerReviewId);
-    if (!customerReview) {
-      next({
-        error: "NoCustomerReviewError",
-        message: `A review with name ${customerReview} does not exist`,
-        name: `${customerReview}`,
-      });
-    }
-    try {
-      const customerReviewToDelete = await deleteCustomerReview(
-        customerReviewId
-      );
-      res.send(customerReviewToDelete);
-    } catch (error) {
-      next(error);
-    }
+  const customerReview = await deleteCustomerReview(customerReviewId);
+  if (!customerReview) {
+    next({
+      error: "NoCustomerReviewError",
+      message: `A review with name ${customerReview} does not exist`,
+      name: `${customerReview}`,
+    });
   }
-);
+  try {
+    const customerReviewToDelete = await deleteCustomerReview(customerReviewId);
+    res.send(customerReviewToDelete);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // I think this is more of an admin thing??
 
