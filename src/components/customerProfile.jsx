1150ab0f70d21c198aa54666046699c7ebd7e9a8
blastedkiwi <krys.orit@gmail.com>
2023-01-27 08:46:26 -0600

    register, home and customer profile

---
+++ src/components/customerProfile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import {Navlink} from "react-router-dom;"
+// need to import my navbar here
+
+const Customerprofile = ({ setToken, user }) => {
+    return (
+        <div className="custprof">
+            <header>
+                {/* <My Navbar component here, setToken={setToken} /> */}
+            </header>
+            Customer Profile here, will add later
+        </div>
+    )
+}
+
+export default Customerprofile
