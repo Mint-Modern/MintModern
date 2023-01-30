6e78ab29b2c0ab60d141ae1f23c1efca4e26d482
Collin Weaver <weaverscajunrenovations@gmail.com>
2023-01-30 08:54:47 -0600

    fixed typos

--- src/components/vermicelliBowl.jsx
+++ src/components/vermicelliBowl.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import MenuNav from "./menuNav";
 
-const VermicelliBowl = ({ products }) => {
+const VermicilliBowl = ({ products }) => {
   let productsToMap = products.map((product, index) => {
-    if (product.category === "vermicelliBowls")
+    if (product.category === "vermicilliBowl")
       return (
         <div className="single-prod" key={index}>
           <h4 className="prod-name">{product.name}</h4>
-          <h5><i>{product.description}</i></h5>
-          <h5>| {product.price} |</h5>  
+          <h5>
+            <i>{product.description}</i>
+          </h5>
+          <h5>| {product.price} |</h5>
         </div>
       );
   });
@@ -17,13 +19,21 @@ const VermicelliBowl = ({ products }) => {
     <>
       <MenuNav />
       <h2 className="prod-cat">
-        <img src="https://i.ibb.co/CQdWn1w/Page-Break-Left-1-2.png" alt="Page-Break-Left-1-2" border="0"/>
-          Vermicelli Bowls
-        <img src="https://i.ibb.co/x1VqQK5/Page-Break-Right-1-2.png" alt="Page-Break-Right-1-2" border="0"/>
+        <img
+          src="https://i.ibb.co/CQdWn1w/Page-Break-Left-1-2.png"
+          alt="Page-Break-Left-1-2"
+          border="0"
+        />
+        Vermicelli Bowls
+        <img
+          src="https://i.ibb.co/x1VqQK5/Page-Break-Right-1-2.png"
+          alt="Page-Break-Right-1-2"
+          border="0"
+        />
       </h2>
       <div className="products">{productsToMap}</div>
     </>
   );
 };
 
-export default VermicelliBowl;
+export default VermicilliBowl;
