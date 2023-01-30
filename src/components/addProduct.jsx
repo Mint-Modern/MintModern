d5fe2e28844b71294dd69b4feeeb9d9305e918bb
Collin Weaver <weaverscajunrenovations@gmail.com>
2023-01-30 12:04:43 -0600

    addProduct component started

--- src/components/addProduct.jsx
+++ src/components/addProduct.jsx
@@ -1,12 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { getAllProducts } from "../../db";
+import { attachProductToOrder, getAllOrders } from "../api/auth";
 
-const AddProduct = ({ order, setOrder }) => {
+const AddProduct = ({ order, setOrder, product }) => {
   const orderId = order.id;
-  const [productId, setProductId] = useState("");
+  const productId = product.id;
   const [quantity, setQuantity] = useState("");
 
-  return <div></div>;
+  return (
+    <div>
+      <form
+        onSubmit={async (event) => {
+          try {
+            event.preventDefault();
+            const addProduct = await attachProductToOrder({
+              orderId,
+              productId,
+              quantity,
+            });
+
+            const orders = await getAllOrders();
+            const orderToFilter = orders?.filter((order) => {
+              return addProduct.orderId === order.id;
+            });
+            setOrder(orderToFilter[0]);
+          } catch {
+            console.error(error);
+          }
+        }}
+      >
+        <label htmlFor="quantity">Quantity</label>
+        <input
+          value={quantity}
+          type="number"
+          placeholder="quantity"
+          min="1"
+          onChange={(event) => setQuantity(parseInt(event.target.value))}
+        ></input>
+        <button type="submit">Add to Cart</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddProduct;
