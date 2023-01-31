e8e5c205c36a8ce18ee57d8d3f8e654101cc8368
Collin Weaver <weaverscajunrenovations@gmail.com>
2023-01-31 14:33:02 -0600

    imported missing function

--- src/components/houseSpecials.jsx
+++ src/components/houseSpecials.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import MenuNav from "./menuNav";
+import { attachProductToOrder } from "../api/auth";
 
 const HouseSpecials = ({ products }) => {
   const navigate = useNavigate();
