5a701442c6b8b3adb722c142328bcaae75c6ebd0
Maria Tran <mp.tran828@gmail.com>
2023-02-07 13:24:31 -0600

    finaleditDrinks

--- src/components/drinks.jsx
+++ src/components/drinks.jsx
@@ -1,141 +1,102 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import MenuNav from "./menuNav";
-// import { attachProductToOrder } from "../api/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import MenuNav from "./menuNav";
+import { attachProductToOrder } from "../api/auth";
 
-// const Drinks = ({ products, user }) => {
-//   const navigate = useNavigate;
-//   const [value, setValue] = useState("");
+const Drinks = ({ products, user }) => {
+console.log("drinkproducts", products)
+const navigate = useNavigate();
+const [value, setValue] = useState("");
+const [selectedOption, setSelectedOption] = useState(null);
 
-//   const handleChange = (e) => {
+// const handleChange = (e) => {
 //     setValue(e.target.value);
-//   };
-
-//   let productsToMap = products?.map((product, index) => {
-//     const optionsSmoothie = product.description?.split(", ");
-//     const optionsMilkTea = product.description?.split(", ");
-   
-//     if (product.category === "smoothies")
-//       return (
-//         <div className="single-prod" key={index}>
-//           <h4
-//             className="prod-name"
-//             onClick={() => {
-//               navigate(`/products/${product.id}`);
-//             }}
-//           >
-//             {product.name}
-//           </h4>
-//           <h5>
-//             <i>{product.description}</i>
-//           </h5>
-//           <h5>
-//             <select
-//               value={value}
-//               multiple={false}
-//               key={index}
-//               onChange={handleChange}
-//             >
-//               <option>Please Choose Your Flavor</option>
-//               {optionsSmoothie.map((option, index) => {
-//                 return (
-//                   <option value={option} key={index}>
-//                     {option}
-//                   </option>
-//                 );
-//               })}
-//             </select>
-//           </h5>
-//           <h5>| {product.price} |</h5>
-//           <button
-//             onClick={async () => {
-//               user.name
-//                 ? await attachProductToOrder({ productId: product.id })
-//                 : addProductToLocalCart(product);
-//             }}
-//           >
-//             Add to cart!
-//           </button>
-//         </div>
-//       );
-
-//     if (product.category === "milkTeas") {
-//       return (
-//         <div className="single-prod" key={index}>
-//           <h4
-//             className="prod-name"
-//             onClick={() => {
-//               navigate(`/products/${product.id}`);
-//             }}
-//           >
-//             {product.name}
-//           </h4>
-//           <h5>
-//             <i>{product.description}</i>
-//           </h5>
-//           <h5>
-//             <select
-//               value={value}
-//               multiple={false}
-//               key={index}
-//               onChange={handleChange}
-//             >
-//               <option>Please Choose Your Flavor</option>
-//               {optionsMilkTea.map((option, index) => {
-//                 return (
-//                   <option value={option} key={index}>
-//                     {option}
-//                   </option>
-//                 );
-//               })}
-//             </select>
-//           </h5>
-//           <h5>| {product.price} |</h5>
-//           <button
-//             onClick={async () => {
-//               user.name
-//                 ? await attachProductToOrder({ productId: product.id })
-//                 : addProductToLocalCart(product);
-//             }}
-//           >
-//             Add to cart!
-//           </button>
-//         </div>
-//       );
-//     }
-    
-//     const addProductToLocalCart = (product) => {
-//       let cart = JSON.parse(localStorage.getItem("cart")) || {
-//         products: [],
-//         isActive: true,
-//         salesTax: 0.0945,
-//         total: 0,
-//       };
-//       cart.products.push(product);
-//       localStorage.setItem("cart", JSON.stringify(cart));
-//     };
-
-//     return (
-//       <>
-//         <MenuNav />
-//         <h2 className="prod-cat">
-//           <img
-//             src="https://i.ibb.co/CQdWn1w/Page-Break-Left-1-2.png"
-//             alt="Page-Break-Left-1-2"
-//             border="0"
-//           />
-//           Drinks
-//           <img
-//             src="https://i.ibb.co/x1VqQK5/Page-Break-Right-1-2.png"
-//             alt="Page-Break-Right-1-2"
-//             border="0"
-//           />
-//         </h2>
-//         <div className="products">{productsToMap}</div>
-//       </>
-//     );
-//   });
-  
 // };
 
-// export default Drinks;
+const handleChange = (e) => {
+    setSelectedOption(e.target.selectedOption)
+}
+
+
+  let productsToMap = products?.map((product, index) => {
+    const optionsTapioca = product.description?.split(", ");
+    if (product.category === "tapiocas")
+      return (
+        <div className="single-prod" key={index}>
+          <div
+            className="top"
+            onClick={() => {
+              navigate(`/products/${product.id}`);
+            }}
+          >
+            <img src={product.image} className="prod-photo" />
+            <h4 className="prod-name">{product.name}</h4>
+          </div>
+          <h5 className="content">
+            <i>{product.description}</i>
+          </h5>
+          <h5>
+             <select
+              value={value || "Please Choose Your Flavor"}
+              multiple={false}
+              key={index}
+              onChange={handleChange}
+            >
+              {optionsTapioca.map((option, index) => {
+                return (
+                  <option value={option} key={index}>
+                    {option}
+                  </option>
+                );
+              })}
+            </select>
+          </h5>
+          <h5 className="content">| {product.price} |</h5>
+          <img
+            src="https://i.ibb.co/642vNF2/add-icon-v2.png"
+            alt="add-icon-v2"
+            className="add-icon"
+            onClick={async () => {
+              user.name
+                ? await attachProductToOrder({ productId: product.id })
+                : addProductToLocalCart(product);
+            }}
+          />
+        </div>
+      );
+  });
+
+  const addProductToLocalCart = (product) => {
+    let cart = JSON.parse(localStorage.getItem("cart")) || {
+      products: [],
+      isActive: true,
+      salesTax: 0.0945,
+      total: 0,
+    };
+    let updatedProduct = { ...product, selectedOption };
+    cart.products.push(updatedProduct);
+    localStorage.setItem("cart", JSON.stringify(cart));
+  };
+
+  return (
+    <>
+      <MenuNav />
+      <h2 className="prod-cat">
+        <img
+          src="https://i.ibb.co/CQdWn1w/Page-Break-Left-1-2.png"
+          alt="Page-Break-Left-1-2"
+          border="0"
+        />
+        Drinks
+        <img
+          src="https://i.ibb.co/x1VqQK5/Page-Break-Right-1-2.png"
+          alt="Page-Break-Right-1-2"
+          border="0"
+        />
+      </h2>
+      <div className="products">{productsToMap}</div>
+    </>
+  );
+};
+
+export default Drinks;
